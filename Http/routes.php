ca8019183abb5e89679bc4b60dcd686c74c478f5
Felipe da Silva Pinheiro <felipepinheiro@univicosa.com.br>
2017-11-10 10:48:22 -0200

    Cors enabled

--- Http/routes.php
+++ Http/routes.php
@@ -2,7 +2,8 @@
 
 Route::middleware('web')->namespace('Modules\OpenId\Http\Controllers')->group(function()
 {
-    Route::get('login', 'LoginController@login')->name('login');
+    Route::get('login', 'LoginController@login')
+        ->middleware(\Modules\OpenId\Http\Middleware\CorsMiddleware::class)->name('login');
 
     Route::get('logout', 'LoginController@logout')->name('logout');
 
